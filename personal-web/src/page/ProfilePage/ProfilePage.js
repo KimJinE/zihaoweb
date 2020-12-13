6168bbedcfff0843ab5d3fffd7bcd531723b6445
kimjin <jinente@udel.edu>
2020-12-13 17:31:10 -0500

    profile

--- personal-web/src/page/ProfilePage/ProfilePage.js
+++ personal-web/src/page/ProfilePage/ProfilePage.js
@@ -1,12 +1,21 @@
 import React from "react";
 import HeaderNav from "../../Component/Header/HeaderNav";
+import "./ProfilePage.css"
+import image from"./../../picture/profile.jpeg"
 
 export default class ProfilePage extends React.Component {
     render() {
         return(
-            <div>
-                <HeaderNav hrId="homehr" />
-                <p>profilepage</p>
+            <div id='Profile-container'>
+                <div id="homeheader">
+                    <HeaderNav/>
+                </div>
+                {/* <div id='Topborder'></div> */}
+                <div id='profile-img'>
+                </div>
+                <div id='profile-mainpart'>
+                    <div></div>
+                </div>
             </div>
         );
     }
